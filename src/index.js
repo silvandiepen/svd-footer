3f931108634d0118882b2324404557818501b9ff
Sil <sil@matise.nl>
2018-04-26 17:23:20 +0200

    new way to try

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import Vue from "vue";
-import svdFooter from "./svd-footer/index.vue";
-
-module.exports = {
-  install: function(Vue, options) {
-    Vue.component("svd-footer", svdFooter);
-  }
-};
